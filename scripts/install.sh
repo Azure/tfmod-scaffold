c0d963f58bb7410de8f59a017f539c756f2f43f8
zjhe <hezijie@microsoft.com>
2022-08-01 11:25:29 +0800

    add install script

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export BRANCH=${1:-main}
+export GITHUB_REPO="https://github.com/lonegunmanb/tfmod-scaffold.git"
+
+if [ -d "tfmod-scaffold" ]; then
+  echo "Removing existing scaffold"
+  rm -rf "tfmod-scaffold"
+  rm -rf "scripts"
+fi
+
+echo "Cloning tfmod-scaffold"
+git clone -c advice.detachedHead=false --depth=1 -b $BRANCH $GITHUB_REPO
