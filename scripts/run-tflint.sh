1e6f29a2f9c7629773f8406cade7d10695518ed0
zjhe <hezijie@microsoft.com>
2022-08-03 14:09:52 +0800

    we can assign the tflint config file to use if we don't like the file copied from this centralized repo.

--- scripts/run-tflint.sh
+++ scripts/run-tflint.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+
+if [ -z "${TFLINT_CONFIG}" ]; then
+	export TFLINT_CONFIG="./tflint.hcl"
+fi
+
 # Check gofmt
 echo "==> Checking that code complies with tflint requirements..."
-tflint --init
+tflint --init --config=$TFLINT_CONFIG
 files=$(find * -maxdepth 1 -type f -name "*.tf" | grep -v ".terraform")
 error=false
 for f in $files; do
-  tflint "$f" || error=true
+  tflint  --config=$TFLINT_CONFIG "$f" || error=true
 done
 if ${error}; then
   echo "------------------------------------------------"
@@ -14,11 +19,17 @@ if ${error}; then
   echo ""
   exit 1
 fi
+
+if [ ! -d "examples" ]; then
+	echo "===> No examples folder, skip lint example code"
+	exit 0
+fi
+
 cd examples
 files=$(find * -type f -name "*.tf" | grep -v ".terraform")
 error=false
 for f in $files; do
-  tflint --config=../.tflint.hcl --disable-rule=terraform_documented_outputs --disable-rule=terraform_documented_variables "$f" || error=true
+  tflint --config=../$TFLINT_CONFIG --disable-rule=terraform_documented_outputs --disable-rule=terraform_documented_variables "$f" || error=true
 done
 if ${error}; then
   echo "------------------------------------------------"
