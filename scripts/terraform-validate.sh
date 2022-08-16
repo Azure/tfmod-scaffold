c80137bb084b2fa5e00f6bb687e734874c7be48a
Saverio Proto <saverioproto@microsoft.com>
2022-08-16 15:39:14 +0200

    Exit and first validation error. Provide context to debug
    https://github.com/tfmodtest/terraform-azurerm-aks/pull/6/files

--- scripts/terraform-validate.sh
+++ scripts/terraform-validate.sh
@@ -6,12 +6,13 @@ echo "==> Checking examples terraform codes are validate..."
 examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
 for d in $examples; do
   (echo "===> Terraform validating in " $d && cd $d && rm -f .terraform.lock.hcl && terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
+  if ${error}; then
+     echo "------------------------------------------------"
+     echo ""
+     echo "Some Terraform codes contain errors."
+     echo "$(cd $d && terraform validate -json)"
+     echo ""
+     exit 1
+  fi
 done
-if ${error}; then
-  echo "------------------------------------------------"
-  echo ""
-  echo "Some Terraform codes contain errors."
-  echo ""
-  exit 1
-fi
 exit 0
