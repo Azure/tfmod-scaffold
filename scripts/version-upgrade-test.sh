701d371971662c01cdc1ace94429a0b85dd3e610
hezijie <lonegunmanb@hotmail.com>
2022-08-23 17:02:50 +0800

    set PREVIOUS_MAJOR_VERSION when it's absent

--- scripts/version-upgrade-test.sh
+++ scripts/version-upgrade-test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 echo "==> Running Module Version Upgrade Tests..."
 set -e
-export PREVIOUS_MAJOR_VERSION=$(ls | grep CHANGELOG |  cut -d'-' -f 2 | cut -f 1 -d '.' |  grep v | sort -V -r | head -n 1)
+if [ -z "$PREVIOUS_MAJOR_VERSION" ]; then
+    export PREVIOUS_MAJOR_VERSION=$(ls | grep CHANGELOG |  cut -d'-' -f 2 | cut -f 1 -d '.' |  grep v | sort -V -r | head -n 1)
+fi
 cd ./test/upgrade
 go mod tidy
 echo "==> Executing go test"
