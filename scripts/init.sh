7aa52dec133c38ee469ec0897c1f201adbb3346f
zjhe <hezijie@microsoft.com>
2022-09-02 11:02:05 +0800

    Add issue template provided by @zioproto(Thanks!)

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,10 @@ if [ -z "${SKIP_INSTALL_GH_ACTION_WORKFLOW}" ]; then
   cp tfmod-scaffold/workflows/acc-test.yaml .github/workflows/acc-test.yaml
   cp tfmod-scaffold/workflows/pr-check.yaml .github/workflows/pr-check.yaml
   cp tfmod-scaffold/workflows/main-branch-push.yaml .github/workflows/main-branch-push.yaml
+  mkdir -p .github/ISSUE_TEMPLATE
+  cp tfmod-scaffold/workflows/ISSUE_TEMPLATE/Bug_Report.yml .github/ISSUE_TEMPLATE/Bug_Report.yml
+  cp tfmod-scaffold/workflows/ISSUE_TEMPLATE/Feature_Request.yml .github/ISSUE_TEMPLATE/Feature_Request.yml
+  cp tfmod-scaffold/workflows/ISSUE_TEMPLATE/config.yml .github/ISSUE_TEMPLATE/config.yml
 else
 	echo "Skip workflow installation"
 fi
