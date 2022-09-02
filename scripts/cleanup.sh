1775e0fa0501311c7da0d066bd683bb5b3ab710c
zjhe <hezijie@microsoft.com>
2022-09-02 15:01:39 +0800

    clean issue templates in cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,6 +2,9 @@
 rm -f .github/workflows/acc-test.yaml
 rm -f .github/workflows/pr-check.yaml
 rm -f .github/workflows/main-branch-push.yaml
+rm -f .github/ISSUE_TEMPLATE/Bug_Report.yml
+rm -f .github/ISSUE_TEMPLATE/Feature_Request.yml
+rm -f .github/ISSUE_TEMPLATE/config.yml
 rm -f .tflint.hcl
 rm -rf "tfmod-scaffold"
 rm -rf "scripts"
