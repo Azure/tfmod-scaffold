68bb52eb4fdbd1abb011e14ca0bbf8041b953a59
zjhe <hezijie@microsoft.com>
2022-09-02 14:33:27 +0800

    prepare transfer to Azure org

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export BRANCH=${1:-main}
-export GITHUB_REPO="https://github.com/lonegunmanb/tfmod-scaffold.git"
+export GITHUB_REPO="https://github.com/Azure/tfmod-scaffold.git"
 if [ -d "tfmod-scaffold" ]; then
   echo "Removing existing scaffold"
   rm -rf "tfmod-scaffold"
