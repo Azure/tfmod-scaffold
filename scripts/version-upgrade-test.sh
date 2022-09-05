87522b843304170816fb4a149812059c31df0465
zjhe <hezijie@microsoft.com>
2022-09-05 14:42:48 +0800

    skip version-upgrade test when the test folder is absent

--- scripts/version-upgrade-test.sh
+++ scripts/version-upgrade-test.sh
@@ -4,7 +4,12 @@ set -e
 if [ -z "$PREVIOUS_MAJOR_VERSION" ]; then
     export PREVIOUS_MAJOR_VERSION=$(ls | grep CHANGELOG |  cut -d'-' -f 2 | cut -f 1 -d '.' |  grep v | sort -V -r | head -n 1)
 fi
+if [ ! -d "test/upgrade" ]; then
+  echo "No version-upgrade test directory yet, skip test"
+  exit 0
+fi
 cd ./test/upgrade
 go mod tidy
+go mod vendor
 echo "==> Executing go test"
-go test -v -p=1 -timeout=120m ./...
+go test -v -p=1 -timeout=240m ./...
