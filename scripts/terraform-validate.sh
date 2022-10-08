fcdbc6f4ade8cebfdc0641fd8a3653b82d5ce84e
zjhe <hezijie@microsoft.com>
2022-10-08 15:30:39 +0800

    delete .terraform before run tf validate

--- scripts/terraform-validate.sh
+++ scripts/terraform-validate.sh
@@ -5,7 +5,7 @@ error=false
 echo "==> Checking examples terraform codes are validate..."
 examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
 for d in $examples; do
-  (echo "===> Terraform validating in " $d && cd $d && rm -f .terraform.lock.hcl && terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
+  (echo "===> Terraform validating in " $d && cd $d && rm -f .terraform.lock.hcl && rm -rf .terraform && terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
   if ${error}; then
      echo "------------------------------------------------"
      echo ""
