13a7ab6008543fa5251a5a577353e9812a22dc2c
zjhe <hezijie@microsoft.com>
2022-10-20 14:40:22 +0800

    update cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -f .github/workflows/acc-test.yaml
 rm -f .github/workflows/pr-check.yaml
+rm -f .github/workflows/breaking-change-detect.yaml
 rm -f .github/workflows/main-branch-push.yaml
 rm -f .github/ISSUE_TEMPLATE/Bug_Report.yml
 rm -f .github/ISSUE_TEMPLATE/Feature_Request.yml
