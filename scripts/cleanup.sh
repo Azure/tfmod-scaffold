6bdbaeb8ec19687983522278e9f001e2f5c513bc
zjhe <hezijie@microsoft.com>
2022-11-10 15:29:13 +0800

    add new pr template and auto update changelog ci action

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,9 +4,11 @@ rm -f .github/workflows/acc-test.yaml
 rm -f .github/workflows/pr-check.yaml
 rm -f .github/workflows/breaking-change-detect.yaml
 rm -f .github/workflows/main-branch-push.yaml
+rm -f .github/workflows/update-changelog.yaml
 rm -f .github/ISSUE_TEMPLATE/Bug_Report.yml
 rm -f .github/ISSUE_TEMPLATE/Feature_Request.yml
 rm -f .github/ISSUE_TEMPLATE/config.yml
+rm -f .github/pull_request_template.md
 rm -f .tflint.hcl
 rm -f .tflint_example.hcl
 rm -rf "tfmod-scaffold"
--- scripts/init.sh
+++ scripts/init.sh
@@ -5,10 +5,13 @@ if [ -z "${SKIP_INSTALL_GH_ACTION_WORKFLOW}" ]; then
   cp tfmod-scaffold/workflows/pr-check.yaml .github/workflows/pr-check.yaml
   cp tfmod-scaffold/workflows/main-branch-push.yaml .github/workflows/main-branch-push.yaml
   cp tfmod-scaffold/workflows/breaking-change-detect.yaml .github/workflows/breaking-change-detect.yaml
+  cp tfmod-scaffold/workflows/update-changelog.yaml .github/workflows/update-changelog.yaml
   mkdir -p .github/ISSUE_TEMPLATE
   cp tfmod-scaffold/workflows/ISSUE_TEMPLATE/Bug_Report.yml .github/ISSUE_TEMPLATE/Bug_Report.yml
   cp tfmod-scaffold/workflows/ISSUE_TEMPLATE/Feature_Request.yml .github/ISSUE_TEMPLATE/Feature_Request.yml
   cp tfmod-scaffold/workflows/ISSUE_TEMPLATE/config.yml .github/ISSUE_TEMPLATE/config.yml
+
+  cp tfmod-scaffold/workflows/pull_request_template.md .github/pull_request_template.md
 else
 	echo "Skip workflow installation"
 fi
