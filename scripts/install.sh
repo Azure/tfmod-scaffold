79b96ac7127941d57641b2abe11941c54d72418f
zjhe <hezijie@microsoft.com>
2022-12-15 18:05:35 +0800

    rollback init and clean

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 export BRANCH=${1:-main}
 export GITHUB_REPO="https://github.com/Azure/tfmod-scaffold.git"
-echo "cleanup"
-make cleanup
 echo "Cloning tfmod-scaffold"
 git clone -q -c advice.detachedHead=false --depth=1 -b $BRANCH $GITHUB_REPO
 ln -s tfmod-scaffold/scripts scripts
 cp -r tfmod-scaffold/.tflint.hcl .tflint.hcl
 cp -r tfmod-scaffold/.tflint_example.hcl .tflint_example.hcl
-echo "init"
-make init
