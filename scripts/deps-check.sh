80435e7d944f209681b65dc1d1b77e4cc82a2707
zjhe <hezijie@microsoft.com>
2022-12-15 09:31:06 +0800

    do not check go.sum in deps-check so we can merge dependabot's pr directly

--- scripts/deps-check.sh
+++ scripts/deps-check.sh
@@ -6,13 +6,10 @@ fi
 cd ./test
 echo "==> Checking source code with go mod tidy..."
 cp go.mod go_mod
-cp go.sum go_sum
 go mod tidy
 echo "==> Checking source code with go mod vendor..."
 go mod vendor
 diff -q go.mod go_mod || \
   (echo; echo "Unexpected difference in go.mod. Run 'make depsensure' to update the generated document and commit."; rm go_mod go_sum;exit 1)
-diff -q go.sum go_sum || \
-  (echo; echo "Unexpected difference in go.sum. Run 'make depsensure' to update the generated document and commit."; rm go_mod go_sum;exit 1)
 
-rm go_mod go_sum
+rm go_mod
