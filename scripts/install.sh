d42b75c053b46f606269703b3bfcca65bd59a02a
zjhe <hezijie@microsoft.com>
2022-12-15 18:30:17 +0800

    fix install, init and cleanup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export BRANCH=${1:-main}
 export GITHUB_REPO="https://github.com/Azure/tfmod-scaffold.git"
+if [ -d "tfmod-scaffold" ]; then
+  echo "Removing existing scaffold"
+  rm -rf "tfmod-scaffold"
+  rm -rf "scripts"
+fi
 echo "Cloning tfmod-scaffold"
 git clone -q -c advice.detachedHead=false --depth=1 -b $BRANCH $GITHUB_REPO
 ln -s tfmod-scaffold/scripts scripts
