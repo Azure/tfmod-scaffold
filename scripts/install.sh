34ba039158ef073766793f91d69d41ddb40695cb
zjhe <hezijie@microsoft.com>
2022-12-28 09:31:54 +0800

    add devcontainer config

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,3 +11,7 @@ git clone -q -c advice.detachedHead=false --depth=1 -b $BRANCH $GITHUB_REPO
 ln -s tfmod-scaffold/scripts scripts
 cp -r tfmod-scaffold/.tflint.hcl .tflint.hcl
 cp -r tfmod-scaffold/.tflint_example.hcl .tflint_example.hcl
+
+rm -rf .devcontainer
+mkdir -p .devcontainer
+cp tfmod-scaffold/devcontainer/devcontainer.json .devcontainer/devcontainer.json
