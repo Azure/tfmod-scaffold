3c57edcd8ef30ca63f943d17f93d9f2763cc1145
hezijie <lonegunmanb@hotmail.com>
2023-03-07 13:46:33 +0800

    try to fix update testrecord issue

--- scripts/ci-update-test-record.sh
+++ scripts/ci-update-test-record.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-git config --global --add safe.directory /src
+cd .git
+sudo chmod -R a+rwX .
+sudo find . -type d -exec chmod g+s '{}' +
+cd ..
+git config --global --add safe.directory '*'
 sudo sh scripts/update-test-record.sh
