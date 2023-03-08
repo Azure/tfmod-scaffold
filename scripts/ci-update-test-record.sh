fb130b5e9fae4068a02928b25ff59f162c0ebef7
zjhe <hezijie@microsoft.com>
2023-03-08 08:31:44 +0800

    try to fix weekly e2e test update test record job

--- scripts/ci-update-test-record.sh
+++ scripts/ci-update-test-record.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 git config --global --add safe.directory '*'
 docker run --rm -v $(pwd):/src -w /src mcr.microsoft.com/azterraform sh scripts/update-test-record.sh
+
+cd .git
+sudo chmod -R a+rwX .
+sudo find . -type d -exec chmod g+s '{}' +
