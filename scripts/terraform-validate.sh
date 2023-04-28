ceb7514e18eea619d315ed43287f3d12379c0cca
zjhe <hezijie@microsoft.com>
2023-04-28 22:45:32 +0800

    correct terraform-validate

--- scripts/terraform-validate.sh
+++ scripts/terraform-validate.sh
@@ -6,7 +6,7 @@ if ${error}; then
      echo "------------------------------------------------"
      echo ""
      echo "Some Terraform codes contain errors."
-     echo "$(cd $d && terraform validate -json)"
+     echo "$(terraform validate -json)"
      echo ""
      exit 1
 fi
