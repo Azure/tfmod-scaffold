1b4ec99545d763eddbe73f025eb213030b668375
zjhe <hezijie@microsoft.com>
2023-04-28 22:45:14 +0800

    correct terraform-validate

--- scripts/terraform-validate.sh
+++ scripts/terraform-validate.sh
@@ -2,6 +2,14 @@
 echo "==> Validating module terraform codes..."
 error=false
 (terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
+if ${error}; then
+     echo "------------------------------------------------"
+     echo ""
+     echo "Some Terraform codes contain errors."
+     echo "$(cd $d && terraform validate -json)"
+     echo ""
+     exit 1
+fi
 echo "==> Checking examples terraform codes are validate..."
 examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
 for d in $examples; do
