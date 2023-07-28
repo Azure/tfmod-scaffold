5c961d02ef70191838e58dc2b4bc8092a6f09158
hezijie <lonegunmanb@hotmail.com>
2023-07-28 09:25:10 +0800

    map runner's docker socket into test container

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -2,5 +2,5 @@ set -e
 az login --identity --username $MSI_ID > /dev/null
 export ARM_SUBSCRIPTION_ID=$(az login --identity --username $MSI_ID | jq -r '.[0] | .id')
 export ARM_TENANT_ID=$(az login --identity --username $MSI_ID | jq -r '.[0] | .tenantId')
-docker run --rm -v $(pwd):/src -w /src --network=host -e MSI_ID -e ARM_SUBSCRIPTION_ID -e ARM_TENANT_ID -e ARM_USE_MSI=true mcr.microsoft.com/azterraform:latest make e2e-test
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src -w /src --network=host -e MSI_ID -e ARM_SUBSCRIPTION_ID -e ARM_TENANT_ID -e ARM_USE_MSI=true mcr.microsoft.com/azterraform:latest make e2e-test
 sudo chown -R $USER ./
