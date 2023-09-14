8d4f1cf140b4b7446fad2ee825ab8d5d6c053c4f
hezijie <lonegunmanb@hotmail.com>
2023-09-14 16:42:37 +0800

    update document generate script, support multiple terraform-docs config files so we can generate multiple document files

--- scripts/gencheck.sh
+++ scripts/gencheck.sh
@@ -4,12 +4,22 @@ echo "==> Generating..."
 cp README.md README-generated.md
 
 if [ -f ".terraform-docs.yml" ]; then
-	terraform-docs -c .terraform-docs.yml --output-file README-generated.md ./
+	terraform-docs -c .terraform-docs.yml --output-file README.md ./
+elif [ -d ".terraform-docs" ]; then
+  configs=$(find .terraform-docs -maxdepth 1 -mindepth 1 -type f)
+  for config in $configs; do
+    terraform-docs -c $config ./
+    config=${config%.yml}
+    config=${config#.terraform-docs/}
+    if [ -f "${config}.md" ]; then
+      markdown-table-formatter ${config}.md
+    fi
+  done
 else
-	terraform-docs markdown table --output-file README-generated.md --output-mode inject ./
+	terraform-docs markdown table --output-file README.md --output-mode inject ./
 fi
 
-markdown-table-formatter README-generated.md
+markdown-table-formatter README.md
 echo "==> Comparing generated code to committed code..."
 diff -q README.md README-generated.md || \
     		(echo; echo "Unexpected difference in generated document. Run 'make pre-commit' to update the generated document and commit."; exit 1)
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,6 +5,16 @@ rm -f .terraform.lock.hcl
 
 if [ -f ".terraform-docs.yml" ]; then
 	terraform-docs -c .terraform-docs.yml --output-file README.md ./
+elif [ -d ".terraform-docs" ]; then
+  configs=$(find .terraform-docs -maxdepth 1 -mindepth 1 -type f)
+  for config in $configs; do
+    terraform-docs -c $config ./
+    config=${config%.yml}
+    config=${config#.terraform-docs/}
+    if [ -f "${config}.md" ]; then
+      markdown-table-formatter ${config}.md
+    fi
+  done
 else
 	terraform-docs markdown table --output-file README.md --output-mode inject ./
 fi
