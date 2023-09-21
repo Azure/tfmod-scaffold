fde2661edbd9e2544552709b02156a98fd506729
hezijie <lonegunmanb@hotmail.com>
2023-09-21 09:47:55 +0800

    stop format markdown table since it could generate a huge markdown

--- scripts/gencheck.sh
+++ scripts/gencheck.sh
@@ -9,17 +9,11 @@ elif [ -d ".terraform-docs" ]; then
   configs=$(find .terraform-docs -maxdepth 1 -mindepth 1 -type f)
   for config in $configs; do
     terraform-docs -c $config ./
-    config=${config%.yml}
-    config=${config#.terraform-docs/}
-    if [ -f "${config}.md" ]; then
-      markdown-table-formatter ${config}.md
-    fi
   done
 else
 	terraform-docs markdown table --output-file README.md --output-mode inject ./
 fi
 
-markdown-table-formatter README.md
 echo "==> Comparing generated code to committed code..."
 diff -q README.md README-generated.md || \
     		(echo; echo "Unexpected difference in generated document. Run 'make pre-commit' to update the generated document and commit."; exit 1)
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -9,14 +9,7 @@ elif [ -d ".terraform-docs" ]; then
   configs=$(find .terraform-docs -maxdepth 1 -mindepth 1 -type f)
   for config in $configs; do
     terraform-docs -c $config ./
-    config=${config%.yml}
-    config=${config#.terraform-docs/}
-    if [ -f "${config}.md" ]; then
-      markdown-table-formatter ${config}.md
-    fi
   done
 else
 	terraform-docs markdown table --output-file README.md --output-mode inject ./
 fi
-
-markdown-table-formatter README.md
