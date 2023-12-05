1c273f889e4d24fd2766aac17878eb8d1c8c2716
hezijie <lonegunmanb@hotmail.com>
2023-12-05 13:59:31 +0800

    try to fix docs-gen

--- avm_scripts/docs-gen.sh
+++ avm_scripts/docs-gen.sh
@@ -6,5 +6,5 @@ terraform-docs -c .terraform-docs.yml .
 echo "==> Generating examples documentation..."
 examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
 for d in $examples; do
-  (echo "===> Generating examples documentation in " $d && cd $d && rm -f .terraform.lock.hcl && terraform-docs .
+  echo "===> Generating examples documentation in " $d && cd $d && rm -f .terraform.lock.hcl && terraform-docs .
 done
