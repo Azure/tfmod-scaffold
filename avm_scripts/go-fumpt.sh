ce51b13d4c17b275f54ec9dbe25fde5b0b59ac85
hezijie <lonegunmanb@hotmail.com>
2023-12-05 14:54:31 +0800

    fix gofumpt target when there's no go file

--- avm_scripts/go-fumpt.sh
+++ avm_scripts/go-fumpt.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-@echo "==> Fixing source code with Gofumpt..."
+echo "==> Fixing source code with Gofumpt..."
 if [ ! -d "test/upgrade" ]; then
   echo "No upgrade test directory yet, skip Gofumpt"
   exit 0
 fi
 set -e
 cd ./test/upgrade
-find . -name '*.go' | grep -v vendor | xargs gofumpt -w
+files=$(find . -name '*.go' | grep -v vendor); if [ -n "$files" ]; then echo "skip" | xargs gofumpt -w; fi
