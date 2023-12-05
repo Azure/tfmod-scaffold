aaf10cfeb0873cac1931613e2b5dae880d9cef29
hezijie <lonegunmanb@hotmail.com>
2023-12-05 15:21:49 +0800

    try to fix golangci-lint script

--- avm_scripts/run-golangci-lint.sh
+++ avm_scripts/run-golangci-lint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 echo "==> Checking test go code against linters..."
-cd ./upgrade
+cd ./tests/upgrade
 go mod tidy
-go mod vendor
 echo "run golangci-lint ..."
 golangci-lint run --timeout 1h ./...
