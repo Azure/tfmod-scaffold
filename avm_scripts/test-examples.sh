45ee18967d6745bce2ee1c16ec4bcd3bbfb04fa0
hezijie <lonegunmanb@hotmail.com>
2023-12-15 13:29:26 +0800

    update avmtest repo url

--- avm_scripts/test-examples.sh
+++ avm_scripts/test-examples.sh
@@ -3,7 +3,7 @@
 cd /tmp
 testFolder="avmtester$RANDOM"
 
-git clone git clone git@github.com:Azure/avmtester.git $testFolder && cd $testFolder
+git clone https://github.com/Azure/avmtester.git $testFolder && cd $testFolder
 go mod tidy
 if [ -z "${TEST_TIMEOUT}" ]; then
     export TEST_TIMEOUT=720m
