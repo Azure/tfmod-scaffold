c8081d864160de4ef714f4f4ea6313d10bf23b3a
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-01-03 11:20:48 +0000

    fix: exclude generated readme

--- avm_scripts/terrafmt.sh
+++ avm_scripts/terrafmt.sh
@@ -1,10 +1,10 @@
 echo "==> Fixing test and document terraform blocks code with terrafmt..."
 
-files=$(find . -type f -name "*.md" -o -name "*.go" | grep -v -e ".github" -e "-terraform" -e "vendor" -e ".terraform" -e "README.md")
+files=$(find . -type f -name "*.md" -o -name "*.go" | grep -v -e ".github" -e "-terraform" -e "vendor" -e ".terraform" -e "README.md" -e "README-generated.md")
 for f in $files; do
   terrafmt fmt -f "$f"
   retValue=$?
-  if [ $retValue -ne 0 ] && [ $retValue -ne 2 ]; then
+  if [ $retValue -ne 0 ]; then
     echo "------------------------------------------------"
     echo ""
     echo "The preceding files contain terraform blocks that are not correctly formatted or contain errors."
