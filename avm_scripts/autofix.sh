139e91fbe48fbdfb0da1796436b8cbfb6c66e750
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-01-03 11:27:46 +0000

    fix: example path

--- avm_scripts/autofix.sh
+++ avm_scripts/autofix.sh
@@ -3,6 +3,6 @@ avmfix -folder "$(pwd)"
 
 examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
 for d in $examples; do
-  echo "===> Autofix in " $d && cd $d && avmfix -folder "$(pwd)"
+  echo "===> Autofix in " $d && cd $d && avmfix -folder "examples/$(pwd)"
 done
 exit 0
