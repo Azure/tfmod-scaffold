0395d389652af826bea92fa1ab1a95becad077d2
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-01-03 11:44:30 +0000

    feat: remove README-generated.md afterwards

--- avm_scripts/docs-check.sh
+++ avm_scripts/docs-check.sh
@@ -9,3 +9,5 @@ terraform-docs -c .terraform-docs.yml .
 echo "==> Comparing generated code to committed code..."
 diff -q README.md README-generated.md || \
     		(echo; echo "Unexpected difference in generated document. Run 'make pre-commit' to update the generated document and commit."; exit 1)
+
+rm -f README-generated.md
