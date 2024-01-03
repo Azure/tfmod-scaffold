df3b614689d213ec70c6562189d145e50d2604eb
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-01-03 11:42:09 +0000

    Update autofix.sh

--- avm_scripts/autofix.sh
+++ avm_scripts/autofix.sh
@@ -3,6 +3,6 @@ avmfix -folder "$(pwd)"
 
 examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
 for d in $examples; do
-  echo "===> Autofix in $d" && avmfix -folder "examples/$d"
+  echo "===> Autofix in $d" && avmfix -folder "$d"
 done
 exit 0
