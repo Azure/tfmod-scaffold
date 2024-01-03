06d9676fb816e63ab0cc20e340d1adf9c7ff01c8
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-01-03 11:55:17 +0000

    Update docs-check.sh

--- avm_scripts/docs-check.sh
+++ avm_scripts/docs-check.sh
@@ -8,6 +8,6 @@ terraform-docs -c .terraform-docs.yml .
 
 echo "==> Comparing generated code to committed code..."
 diff -q README.md README-generated.md || \
-    		(echo; echo "Unexpected difference in generated document. Run 'make pre-commit' to update the generated document and commit."; exit 1)
+    		(echo; echo "Unexpected difference in generated document. Run 'make pre-commit' to update the generated document and commit."; rm -f README-generated.md; exit 1)
 
 rm -f README-generated.md
