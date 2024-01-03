2038b82fb79226484a1c45652281cb3d8acfaded
hezijie <lonegunmanb@hotmail.com>
2024-01-03 10:15:54 +0800

    update script as matt suggested

--- avm_scripts/docs-gen.sh
+++ avm_scripts/docs-gen.sh
@@ -4,8 +4,8 @@ function generate_docs() {
   local dir=$1
   echo "===> Generating examples documentation in " $dir
   rm -f "$dir/.terraform.lock.hcl"
-  if [ -f "$dir/../../.terraform-docs.yml" ]; then
-    terraform-docs -c "$dir/../../.terraform-docs.yml" "$dir"
+  if [ -f ".terraform-docs.yml" ]; then
+    terraform-docs -c ".terraform-docs.yml" "$dir"
   else
     terraform-docs markdown table --output-file "$dir/README.md" --output-mode inject "$dir"
   fi
@@ -15,7 +15,8 @@ echo "==> Generating module documentation..."
 rm -f .terraform.lock.hcl
 terraform-docs -c .terraform-docs.yml .
 echo "==> Generating examples documentation..."
-examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
-for d in $examples; do
+cd examples
+subexamples=$(find ./ -maxdepth 1 -mindepth 1 -type d)
+for d in subexamples; do
   generate_docs $d
 done
