c7656397d38e622b931e2815ad7c6e2c7b69e1c5
hezijie <lonegunmanb@hotmail.com>
2024-02-04 14:19:27 +0800

    fix post-push-starter by using bash instead of sh, some env like  could only live with bash

--- scripts/post-push-starter.sh
+++ scripts/post-push-starter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 REMOTE_SCRIPT="https://raw.githubusercontent.com/Azure/tfmod-scaffold/main/$SCRIPT_FOLDER$AVMSCRIPT_VERSION"
-curl -H 'Cache-Control: no-cache, no-store' -sSL "$REMOTE_SCRIPT/post-push.sh" | sh -s
+curl -H 'Cache-Control: no-cache, no-store' -sSL "$REMOTE_SCRIPT/post-push.sh" | bash
