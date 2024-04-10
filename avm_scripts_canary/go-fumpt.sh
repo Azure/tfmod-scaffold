c61cf2b9912657170edec54f5cd76717384bf1f6
hezijie <lonegunmanb@hotmail.com>
2024-04-10 10:11:30 +0800

    use gofumpt to fix all *.go files for avm modules

--- avm_scripts_canary/go-fumpt.sh
+++ avm_scripts_canary/go-fumpt.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 echo "==> Fixing source code with Gofumpt..."
-if [ ! -d "tests/upgrade" ]; then
-  echo "No upgrade tests directory yet, skip Gofumpt"
-  exit 0
-fi
 set -e
-cd ./tests/upgrade
 files=$(find . -name '*.go' | grep -v vendor); if [ -n "$files" ]; then echo "skip" | xargs gofumpt -w; fi
