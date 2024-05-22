6219f0544da6bbe0e47c67147ed26ebc998ccda2
Jared Holgate <jaredholgate@microsoft.com>
2024-05-22 12:14:48 +0100

    Add validate for modules

--- avm_scripts/terraform-validate.sh
+++ avm_scripts/terraform-validate.sh
@@ -24,4 +24,24 @@ for d in $examples; do
      exit 1
   fi
 done
+
+echo "==> Checking module terraform code are validate..."
+if [ ! -d modules ]; then
+  echo "==> Warning - no modules directory found"
+else
+  examples=$(find ./modules -maxdepth 1 -mindepth 1 -type d)
+  for d in $examples; do
+    (echo "===> Terraform validating in " $d && cd $d && rm -f .terraform.lock.hcl && rm -rf .terraform && terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
+    if ${error}; then
+      echo "------------------------------------------------"
+      echo ""
+      echo "Some Terraform codes contain errors."
+      pwd
+      echo "$(cd $d && terraform validate -json)"
+      echo ""
+      exit 1
+    fi
+  done
+fi
+
 exit 0
