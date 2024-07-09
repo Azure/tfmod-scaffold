56314dd38ba0f0fc1eebd420a123972cfa9d49a7
hezijie <lonegunmanb@hotmail.com>
2024-07-09 09:38:57 +0800

    skip yor tag for tfvm modules

--- scripts/yor-tag.sh
+++ scripts/yor-tag.sh
@@ -1,36 +1,3 @@
 #!/usr/bin/env bash
-set -e
 
-tracing_tags_enabled=$(hclgrep -x 'variable "tracing_tags_enabled" {@*_}' variables.tf)
-tracing_tags_prefix=$(hclgrep -x 'variable "tracing_tags_prefix" {@*_}' variables.tf)
-
-if [ -z "$tracing_tags_enabled" ] || [ -z "$tracing_tags_prefix" ]; then
-    echo "==> No tracing_tags_enabled or tracing_tags_prefix variable found, skip tagging"
-    exit 0
-fi
-
-if [ -f "module_telemetry.tf" ]; then
-	cat module_telemetry.tf |
-  	  hcledit attribute rm resource.modtm_telemetry.this.nonce |
-  	  hcledit attribute rm resource.modtm_telemetry.this.ephemeral_number |
-  	  hcledit attribute append resource.modtm_telemetry.this.ephemeral_number $RANDOM |
-  	  hcledit attribute set resource.modtm_telemetry.this.lifecycle.ignore_changes "[ephemeral_number]" |
-  	  tee module_telemetry.tf.bak
-	cat module_telemetry.tf.bak > module_telemetry.tf
-	rm module_telemetry.tf.bak
-	make autofix
-	git config --global --add safe.directory $(pwd)
-	git add module_telemetry.tf
-  git commit -m "Auto update for yor tags"
-fi
-
-error=false
-yor tag -d "$(pwd)" --skip-dirs "$(pwd)/examples" --skip-tags git_last_modified_by,git_modifiers --tag-prefix avm_ --parsers Terraform || error=true
-if ${error}; then
-  echo "------------------------------------------------"
-  echo ""
-  echo "The preceding files contain terraform blocks that does not complies with yor requirements."
-  echo ""
-  exit 1
-fi
-yorbox -dir "$(pwd)" -tagsPrefix avm_ -toggleName tracing_tags_enabled -ignoreResourceType modtm_telemetry --boxTemplate '/*<box>*/ (var.{{ .toggleName }} ? { for k,v in /*</box>*/ { yor_trace = 123 } /*<box>*/ : replace(k, "avm_", var.tracing_tags_prefix) => v } : {} ) /*</box>*/'
+echo "skip yor tag"
