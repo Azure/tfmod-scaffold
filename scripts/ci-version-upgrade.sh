786217945b27ba729a6f14c12ce85432571c07d1
hezijie <lonegunmanb@hotmail.com>
2024-09-23 13:42:28 +0800

    correct ci

--- scripts/ci-version-upgrade.sh
+++ scripts/ci-version-upgrade.sh
@@ -1,7 +1,7 @@
 set -e
 export ARM_OIDC_REQUEST_TOKEN=$ACTIONS_ID_TOKEN_REQUEST_TOKEN
 export ARM_OIDC_REQUEST_URL=$ACTIONS_ID_TOKEN_REQUEST_URL
-if [ -z "$ACTIONS_ID_TOKEN_REQUEST_URL" ]; then
+if [ -n "$ACTIONS_ID_TOKEN_REQUEST_URL" ]; then
     export ARM_USE_OIDC=true
 fi
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src -w /src --network=host -e GITHUB_TOKEN -e ARM_SUBSCRIPTION_ID -e ARM_TENANT_ID -e ARM_CLIENT_ID -e ARM_OIDC_REQUEST_TOKEN -e ARM_OIDC_REQUEST_URL -e ARM_USE_OIDC -e TF_IN_AUTOMATION -e TF_VAR_enable_telemetry mcr.microsoft.com/azterraform:latest make version-upgrade-test
