ae6045803b3fbd0d013d39f0845f785cfc7d7f81
hezijie <lonegunmanb@hotmail.com>
2025-02-18 14:12:25 +0800

    add script for conftest

---
+++ avm_scripts/conftest.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+has_error=false
+
+if [ ! -d "examples" ]; then
+  echo "No \`examples\` folder found."
+  exit 0
+fi
+
+cd examples
+
+for d in $(find . -type d); do
+  if ls "$d"/*.tf > /dev/null 2>&1; then
+    cd "$d"
+    echo "==> Checking $d"
+
+    terraform init -input=false >/dev/null 2>&1
+    terraform plan -out=tfplan.binary || has_error=true
+    terraform show -json tfplan.binary > tfplan.json || has_error=true
+
+    if [ -f "exception.rego" ]; then
+      conftest test --all-namespaces --update git::https://github.com/lonegunmanb/policy-library-avmrego.git//policy/Azure-Proactive-Resiliency-Library-v2 -p policy -p exception.rego tfplan.json || has_error=true
+    else
+      conftest test --all-namespaces --update git::https://github.com/lonegunmanb/policy-library-avmrego.git//policy/Azure-Proactive-Resiliency-Library-v2 tfplan.json || has_error=true
+    fi
+
+    cd - >/dev/null 2>&1
+  fi
+done
+
+cd ..
+
+if [ "$has_error" = true ]; then
+  echo "At least one \`examples\` folder failed."
+  exit 1
+fi
+
+echo "All \`examples\` folders passed."
+exit 0
