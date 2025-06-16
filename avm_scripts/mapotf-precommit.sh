030047d2ea1c18dbdba9cb9458d49b41c3ffdf02
hezijie <lonegunmanb@hotmail.com>
2025-06-16 16:53:35 +0800

    map mptf dir into test-avm script

--- avm_scripts/mapotf-precommit.sh
+++ avm_scripts/mapotf-precommit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-MPTF_DIR=${MPTF_DIR:-git::https://github.com/Azure/tfmod-scaffold.git//avm_mapotf/pre_commit}
+MPTF_DIR=${MPTF_DIR:-git::https://github.com/Azure/tfmod-scaffold.git//avm_mapotf}
 
-mapotf transform --mptf-dir $MPTF_DIR --tf-dir .
+mapotf transform --mptf-dir $MPTF_DIR/pre_commit --tf-dir .
 avmfix -folder .
 mapotf clean-backup --tf-dir .
